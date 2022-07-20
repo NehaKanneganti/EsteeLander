606b6c9061419235815df02502b3eb1d02b6ab7d
Neha Kanneganti <neha.kanneganti@gmail.com>
2022-07-20 16:44:06 -0400

    first done

---
+++ promotionline/Models/OnboardingSlide.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingSlide.swift
+//  promotionline
+//
+//  Created by Neha Kanneganti on 7/20/22.
+//
+
+import UIKit
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
